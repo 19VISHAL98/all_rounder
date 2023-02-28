c86896bab2687f6987ab3decd0441b4e0490eb15
19VISHAL98 <vishalkumar8673@gmail.com>
2023-02-28 15:35:30 +0530

    selecter

--- src/page/blog/Blog.jsx
+++ src/page/blog/Blog.jsx
@@ -7,35 +7,29 @@ import Login from './login/Login'
 import Register from './register/Register'
 import AllUser from './users/AllUser'
 const Blog = () => { 
-  // const [state,setState] = useState(false)
-  
-     const [state, setState] = useState([{register: false,login:false ,ChangePassword :false ,ForgetPassword :false ,AllUser:false }])
-     const handleButton=()=>{
-    console.log(state.register)
-    
-      
-     }
+  // const [state,setIndex] = useState(false)
+    let data = {register: false,login:false ,ChangePassword :false ,ForgetPassword :false ,AllUser:false }
+     const [index,setIndex] = useState(0)
+   
   return (
     <>
     <Layout>
     <div className='blog'>
     <div className='links'>
-      <div className='buttons' onClick={ handleButton }> <p className='p1'>Register</p>   </div>
-      <div className='buttons'  > <p className='p1'>login</p>   </div>
-      <div className='buttons'> <p className='p1'>show all user</p>   </div>
-      <div className='buttons'> <p className='p1'>change Password</p>   </div>
-      <div className='buttons'> <p className='p1'>forget Password</p>   </div>
+      <div className={`buttons ${index===0?"buttons-active":""}`} onClick={()=> setIndex(0) }> <p className='p1'>Register</p>   </div>
+      <div className={`buttons ${index===1?"buttons-active":""}`} onClick={()=> setIndex(1) } > <p className='p1'>login</p>   </div>
+      <div className={`buttons ${index===2?"buttons-active":""}`} onClick={()=> setIndex(2) } > <p className='p1'>change Password</p>   </div>
+      <div className={`buttons ${index===3?"buttons-active":""}`} onClick={()=> setIndex(3) } > <p className='p1'>forget Password</p>   </div>
+      <div className={`buttons ${index===4?"buttons-active":""}`} onClick={()=> setIndex(4) } > <p className='p1'>show all user</p>   </div>
     </div>
     <div className='data'>
-     {/* className={`menu ${state?'active-menu':""}`} */}
-     {
-      state.register&&(<Register/>)
-     }
-    {/* <div className={`register ${state.register===true?'register-active':''}`}> <Register/> </div> */}
-    <div className='Login'> <Login/> </div>
-    <div className='ChangePassword'> <ChangePassword/> </div>
-    <div className='ForgetPassword'><ForgetPassword/> </div>
-    <div className='AllUser'>  <AllUser/> </div>
+ 
+    <div hidden={index!==0}> <Register/> </div>
+    <div hidden={index!==1}> <Login/> </div>
+    <div hidden={index!==2}> <ChangePassword/> </div>
+    <div hidden={index!==3}> <ForgetPassword/> </div>
+    <div hidden={index!==4}> <AllUser/> </div>
+
     </div>
     </div>
     </Layout>
